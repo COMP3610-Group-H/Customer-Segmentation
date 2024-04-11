a1498aa9a0f5930f5a5367197da35529f261cbbc
nicholasr74 <nicholasramroop74@gmail.com>
2024-04-10 22:48:27 -0400

    Co-authored-by: NaomiDookeran <NaomiDookeran@users.noreply.github.com>

--- SegmentPredictor.py
+++ SegmentPredictor.py
@@ -1,22 +1,44 @@
 
 from flask import Flask, request, jsonify
 from joblib import load
+import json
 
 app = Flask(__name__)
 
 # Load the saved logistic regression model
 model = load('Model.joblib')
 
+def parseJSON(formData):
+    # data = json.loads(formData)
+    jsonArray = list(formData.values())
+    
+    education_level = formData.get("Education")
+    marital_status = formData.get("Marital_Status")
+
+    print(education_level)  
+    print(marital_status)
+
+    
+
+
+
+
+    return jsonArray
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Extract input features from the POST request
     data = request.get_json()
-    features = data['features']
+
+    features = parseJSON(data)
+    # features = data['features']
     print(features)
 
     # Make a prediction
-    prediction = model.predict([features])
+    # prediction = model.predict([features])
+    prediction = []
+    prediction.append(0)
 
     # Send back the prediction
     return jsonify({'prediction': int(prediction[0])})
